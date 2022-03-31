a57ad61b411f61edbe93e7837010d29048699fd9
naufaladrna08 <naufaladrn90@gmail.com>
2022-03-31 14:56:27 +0700

    test imgui and docking

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <logging.h>
 #include <GLFW/glfw3.h>
 
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
 Camera* camera;
 Renderer* render;
 Renderer* light;
@@ -20,7 +24,7 @@ int main(int argc, char* argv[]) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Tridme Engine", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Tridme Engine", NULL, NULL);
 
 	if (!window) {
 		LOG(ERROR, "Cannot create GLFW window!");
@@ -38,20 +42,31 @@ int main(int argc, char* argv[]) {
 
 	{
 		/* Setup ImGUI context */
-		// IMGUI_CHECKVERSION();
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO(); (void) io;
+		ImGui::StyleColorsDark();
 
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
 
-		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);  
-		glfwSetCursorPosCallback(window, mouse_callback);
+		/* ImGui state */
+		bool show_demo_window = true;
+		bool show_another_window = true;
+		ImVec4 clear_color = ImVec4(0.43f, 0.55f, 0.12f, 1.0f);
+
+		// glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);  
+		// glfwSetCursorPosCallback(window, mouse_callback);
 
 		glEnable(GL_DEPTH_TEST);
 
-		
+
 		Shader* shader 			= new Shader("Color.shader");
 		Shader* lightShader = new Shader("Light.shader");
 		camera = new Camera((float) 800, (float) 600, 0.1f, 1000.0f, 
-                    	  glm::vec3(0.0f, 0.0f, 6.0f));
+                    	  glm::vec3(0.0f, 0.0f, 10.0f));
 		
 		/* Object */
 		render = new Renderer(MESH_TYPE::MESH_CUBE, camera); 
@@ -84,12 +99,52 @@ int main(int argc, char* argv[]) {
 			deltaTime = currentFrame - lastFrame;
 			lastFrame = currentFrame;
 
+			/* ImGui frame */
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
+			if (show_demo_window)
+				ImGui::ShowDemoWindow(&show_demo_window);
+
+			{
+				static float f = 0.0f;
+				static int counter = 0;
+
+				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+				ImGui::Checkbox("Another Window", &show_another_window);
+
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+						counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::End();
+			}
+
+			if (show_another_window) {
+				ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+				ImGui::Text("Hello from another window!");
+				if (ImGui::Button("Close Me"))
+					show_another_window = false;
+				ImGui::End();
+			}
+
+			ImGui::Render();
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-			camera->Move(window, deltaTime)	;
 			render->DrawMesh();
 			light->DrawMesh();
+
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 			glfwPollEvents();
 			glfwSwapBuffers(window);
